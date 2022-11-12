401c45d523c46017a03eac3d9f7cb82fa446547e
Ary-Leal <lealariosto@gmail.com>
2022-11-12 17:59:24 -0300

    implementa escolha do menu

--- man.rb
+++ man.rb
@@ -1,4 +1,29 @@
+
+produtos = [ {"produto A": "R$50.00"} , {"produto B": "R$200.00" },{"produto C": "R$100.00"}]
+
+
 puts "selecione a opcao desejada"
 
 puts "[1] Comprar"
 puts "[2] Sair "
+
+opcao_escolhida = gets.chomp.to_i
+
+
+ 
+    if (opcao_escolhida == 1)
+        puts "Selecione o Produto Desejado"
+        puts "[1] Produto A: R$50.00"
+        puts "[2] Produto B: R$2000.00"
+        puts "[3] Produto C: R$100.00"
+    elsif opcao_escolhida == 2
+
+        puts "Ate Breve!!!!"
+    end
+    
+   
+
+
+
+
+
