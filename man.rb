65c2b30dc2692e917dac3e61c22f99f0fe1698ce
Anderson Marcondes <anderson@marcond.es>
2022-11-12 21:44:34 +0000

    pemite escolha de produto e quantidade

--- man.rb
+++ man.rb
@@ -1,29 +1,38 @@
+produtos = [ 
+    {
+        "nome" => "Produto A",
+        "preco" => 50.0        
+    }, 
+    {
+        "nome" => "Produto B",
+        "preco" => 200.0        
+    }, 
+    {
+        "nome" => "Produto C",
+        "preco" => 100.0        
+    }    
+]
 
-produtos = [ {"produto A": "R$50.00"} , {"produto B": "R$200.00" },{"produto C": "R$100.00"}]
-
-
-puts "selecione a opcao desejada"
+puts "Selecione a opcao desejada:"
 
 puts "[1] Comprar"
 puts "[2] Sair "
 
-opcao_escolhida = gets.chomp.to_i
-
-
- 
-    if (opcao_escolhida == 1)
+opcao_escolhida = gets.chomp.to_i 
+    if opcao_escolhida == 1
         puts "Selecione o Produto Desejado"
         puts "[1] Produto A: R$50.00"
         puts "[2] Produto B: R$2000.00"
         puts "[3] Produto C: R$100.00"
-    elsif opcao_escolhida == 2
 
+        produto_escolhido = gets.chomp.to_i
+
+        print "Digite a quantidade desejada: "
+
+        quantidade = gets.chomp.to_i        
+
+        puts "Produto: #{produtos[produto_escolhido - 1]["nome"]} - R$#{produtos[produto_escolhido - 1]["preco"]}"
+        puts "Quantidade: #{quantidade}"
+    elsif opcao_escolhida == 2
         puts "Ate Breve!!!!"
     end
-    
-   
-
-
-
-
-
