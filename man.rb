fb8bdfab3098e232d0c8837fbe13cbdfb9ccf893
Anderson Marcondes <anderson@marcond.es>
2022-11-12 21:56:34 +0000

    exibe sub-total do produto escolhido

--- man.rb
+++ man.rb
@@ -13,8 +13,9 @@ produtos = [
     }    
 ]
 
-puts "Selecione a opcao desejada:"
+sub_total = 0
 
+puts "Selecione a opcao desejada:"
 puts "[1] Comprar"
 puts "[2] Sair "
 
@@ -22,7 +23,7 @@ opcao_escolhida = gets.chomp.to_i
     if opcao_escolhida == 1
         puts "Selecione o Produto Desejado"
         puts "[1] Produto A: R$50.00"
-        puts "[2] Produto B: R$2000.00"
+        puts "[2] Produto B: R$200.00"
         puts "[3] Produto C: R$100.00"
 
         produto_escolhido = gets.chomp.to_i
@@ -33,6 +34,11 @@ opcao_escolhida = gets.chomp.to_i
 
         puts "Produto: #{produtos[produto_escolhido - 1]["nome"]} - R$#{produtos[produto_escolhido - 1]["preco"]}"
         puts "Quantidade: #{quantidade}"
+
+        preco = produtos[produto_escolhido - 1]["preco"]
+        sub_total += preco * quantidade
+        
+        puts "Sub-total: R$#{sub_total}0"
     elsif opcao_escolhida == 2
         puts "Ate Breve!!!!"
     end
