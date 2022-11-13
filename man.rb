3899606cbe1853d62dfc942fcafe6dbae40da997
Anderson Marcondes <anderson@marcond.es>
2022-11-12 23:11:00 -0300

    limpa a tela entre as operações

--- man.rb
+++ man.rb
@@ -1,5 +1,11 @@
+module UI
+    def UI.clear
+        puts "\e[H\e[2J"
+    end
+end
+
 # Lista de produtos disponíveis para adicionar ao carrinho
-produtos = [ 
+produtos = [
     {
         "nome" => "Produto A",
         "preco" => 50.0        
@@ -17,18 +23,22 @@ produtos = [
 # Variáveis de controle do e-commerce
 sub_total = 0.0
 opcao_menu = 0
-def solicita_input_usuario(converte_inteiro = true)
+
+def solicita_input_usuario(converte_inteiro=true)
    input = gets.chomp
    converte_inteiro ? input.to_i : input
 end
+
 def exibe_menu_principal
+    UI.clear
+
     puts "Selecione a opção desejada:\n"
     puts "[1] Comprar"
     puts "[2] Sair"
 
     print "> "
 
-     solicita_input_usuario  
+    solicita_input_usuario  
 end
 
 def calcula_sub_total(produtos, opcao, quantidade)
@@ -38,7 +48,9 @@ def calcula_sub_total(produtos, opcao, quantidade)
     preco_produto * quantidade     
 end
 
- def selecao_produto(produtos )
+def selecao_produto(produtos)
+    UI.clear
+
     puts "Selecione o Produto Desejado"
 
     produtos.each_with_index do |produto, i|
@@ -47,36 +59,42 @@ end
         preco = produto["preco"].to_s + "0"
 
         puts "[#{numero}] #{nome}: #{preco}"            
-    end        
-    solicita_input_usuario 
- end
+    end
 
- def insere_quantidade
+    solicita_input_usuario 
+end
+
+def insere_quantidade
+    UI.clear
+
     print "Digite a quantidade desejada: "
 
-     solicita_input_usuario         
- end 
+    solicita_input_usuario         
+end 
 
- def exibe_produto_escolhido(produto , quantidade)
+def exibe_produto_escolhido(produto, quantidade)
+    UI.clear
+
     puts "Produto: #{produto["nome"]} - R$#{produto["preco"]}0"
-    puts "Quantidade: #{quantidade}"  
-   
- end
-  opcao_menu = exibe_menu_principal
+    puts "Quantidade: #{quantidade}"     
+end
+
+opcao_menu = exibe_menu_principal
  
 while opcao_menu != 2    
     if opcao_menu == 1       
-        produto_escolhido =  selecao_produto(produtos)
-        quantidade =  insere_quantidade        
-        exibe_produto_escolhido(produtos[produto_escolhido -1] ,quantidade)
+        produto_escolhido = selecao_produto(produtos)
+        quantidade = insere_quantidade        
+        exibe_produto_escolhido(produtos[produto_escolhido-1] ,quantidade)
         sub_total += calcula_sub_total(produtos, produto_escolhido, quantidade)
         
         puts "Sub-total: R$#{sub_total}0"
-        print "Digite 0 para voltar ao menu inicial :"
-        opcao_checkout =  solicita_input_usuario         
+        print "Digite 0 para voltar ao menu inicial: "
 
+        opcao_checkout =  solicita_input_usuario   
         opcao_menu = exibe_menu_principal if opcao_checkout == 0
     end   
 end
 
-puts "Ate Breve!!!!"
+UI.clear
+puts "Ate Breve!!!!\n"
